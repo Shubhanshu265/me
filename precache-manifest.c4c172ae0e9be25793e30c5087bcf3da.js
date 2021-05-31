2c11a8c47821d531b1c9fac17de0c3edbbabef3b
shubhanshu.srivastava <shubhanshu.srivastava@ukg.com>
2021-05-31 22:27:49 +0530

    Updates

---
+++ precache-manifest.c4c172ae0e9be25793e30c5087bcf3da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc5ba1359a7d4f9eea83",
+    "url": "/me/static/css/main.f27da0c2.chunk.css"
+  },
+  {
+    "revision": "bc5ba1359a7d4f9eea83",
+    "url": "/me/static/js/main.bc5ba135.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "a7319ea8c93851b51257adbde3346254",
+    "url": "/me/index.html"
+  }
+];
